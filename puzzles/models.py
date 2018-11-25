ef5a0c368c731c456337ee994752f85b075298b0
eulerscheZahl <eulerschezahl@yandex.com>
2018-11-25 09:45:43 +0100

    search in comments

--- puzzles/models.py
+++ puzzles/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Puzzle(models.Model):
     handle = models.CharField(max_length=40)
     puzzle = models.TextField()
+    comments = models.TextField()
--- puzzles/puzzle_manager.py
+++ puzzles/puzzle_manager.py
@@ -8,10 +8,15 @@ def search(search_text):
     for puzzle in Puzzle.objects.all():
         data = json.loads(puzzle.puzzle)['success']
         hit = True
-        for s in search_text.split():
+        for s in search_text.lower().split():
             subHit = False
             for category in ['inputDescription', 'outputDescription', 'constraints', 'statement', 'title']:
                 if category in data['lastVersion']['data'] and s in data['lastVersion']['data'][category].lower(): subHit = True
+            for test in data['lastVersion']['data']['testCases']:
+                if s in str(test['title']).lower(): subHit = True
+            if puzzle.comments != '':
+                for comment in json.loads(puzzle.comments):
+                    if s in comment['content'].lower(): subHit = True
             if not subHit: hit = False
         if hit:
             data['lastVersion']['statementHTML'] += '</div>'
@@ -30,4 +35,21 @@ def update(handle):
         url = 'https://www.codingame.com/services/ContributionRemoteService/getContribution'
         r = requests.post(url, json=[handle,True])
         puzzle = Puzzle(handle=handle, puzzle=json.dumps(r.json()))
+        puzzle.save()
+    if puzzle.comments == '':
+        comments = []
+        url = 'https://www.codingame.com/services/CommentRemoteService/getFirstLevelComments'
+        url2 = 'https://www.codingame.com/services/CommentRemoteService/getSecondLevelComments'
+        r = requests.post(url, json=[None,json.loads(puzzle.puzzle)['success']['commentableId']])
+        for comment in r.json()['success']:
+            comments.append(comment)
+            if not 'parentCommentId' in comment and comment['responseCount'] > 1:
+                r = requests.post(url2, json=[None,comment['commentId']])
+                for comment2 in r.json()['success']:
+                    comments.append(comment2)
+        commentsUnique = []
+        for c in comments:
+            if not any(c['commentId'] == c2['commentId'] for c2 in commentsUnique):
+                commentsUnique.append(c)
+        puzzle.comments = json.dumps(commentsUnique)
         puzzle.save()

72641435375fb9567e7363789505a6f77b0476e5
eulerscheZahl <eulerschezahl@yandex.com>
2019-06-01 10:21:33 +0200

    allow to search for @

--- codejam/urls.py
+++ codejam/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('search/', views.index, name='index'),
-    re_path(r'^search/(?P<search>[\w\d\-_\.]+)$', views.detail, name='detail'),
+    re_path(r'^search/(?P<search>[\w\d\-_\.@]+)$', views.detail, name='detail'),
     #path('update/', views.update, name='update'),
     #path('update2/', views.update2, name='update2'),
     path('ajax/autocomplete/', views.autocomplete, name='ajax_autocomplete')

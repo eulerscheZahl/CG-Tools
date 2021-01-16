68dfc8965743744eddae5913cad5c81e7dd2ca19
eulerscheZahl <eulerschezahl@yandex.com>
2021-01-16 10:53:33 +0100

    enfore https

--- ReplayAnalyzer/settings.py
+++ ReplayAnalyzer/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'ReplayAnalyzer.urls'
 
 TEMPLATES = [

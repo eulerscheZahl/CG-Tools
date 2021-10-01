4e7760e46bea4ce0a6600b583f01d691e6143204
eulerscheZahl <eulerschezahl@yandex.com>
2021-10-01 15:17:14 +0200

    leaderboards: user-pages

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -52,6 +52,9 @@ def index(request):
     replayGroups = None
     botReplays = None
     users = None
+    user = None
+    bots = None
+    user_matches = {}
     if 'game' in request.GET:
         game = request.GET['game']
         leaderboard = Leaderboard.objects.filter(game=game).first()
@@ -63,9 +66,42 @@ def index(request):
         r = requests.post(url, json=data).json()
         users = r['users']
         users = users[:100]
-    games = Leaderboard.objects.all().values_list("game", flat=True)
 
-    return render(request, 'leaderboards.html', {'game': game, 'users': users, 'replayGroups': replayGroups, 'botReplays': botReplays, 'games': games})
+    if 'user' in request.GET:
+        user = request.GET['user']
+        all_bots = json.loads(Leaderboard.objects.filter(
+            game='bots_per_user').first().botReplays)
+        user_of_bot = {}
+        for bot in all_bots:
+            for g in all_bots[bot]['bots']:
+                for bot_id in all_bots[bot]['bots'][g]:
+                    user_of_bot[bot_id] = all_bots[bot]['username']
+
+        bots = all_bots[user]
+        for g in bots['bots']:
+            user_matches[g] = []
+            leaderboard = Leaderboard.objects.filter(game=g).first()
+            replayGroups = json.loads(leaderboard.replayGroups)
+            botReplays = json.loads(leaderboard.botReplays)
+            for bot in bots['bots'][g]:
+                bots_matches = {'bot': botReplays[str(bot)], 'partners': {}}
+                for group in replayGroups:
+                    if bot not in group:
+                        continue
+                    for gr in group:
+                        if user_of_bot[gr] != user_of_bot[bot]:
+                            bots_matches['partners'][user_of_bot[gr]
+                                                     ] = botReplays[str(gr)]
+                user_matches[g].append(bots_matches)
+        print(user_matches)
+
+    games = sorted(Leaderboard.objects.all().values_list("game", flat=True))
+    games.remove('bots_per_user')
+
+    return render(request, 'leaderboards.html',
+                  {'games': games,
+                   'game': game, 'users': users, 'replayGroups': replayGroups, 'botReplays': botReplays,
+                   'user': user, 'bots': bots, 'user_matches': user_matches})
 
 
 @csrf_exempt
